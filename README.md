086131406597ee6198a53ce267efc09d74b25834
rogerioc <rogeriocsantos@MacBook-Pro-de-rogerio.local>
2020-08-10 20:54:49 -0300

    Initial ans carthage config

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # RedditSampleApp
+
+Need install Carthage - https://github.com/Carthage/Carthage
+
+In console run 
+    
+    $ cd vendor && carthage update --platform iOS 
+to install third party libs 
---
+++ RedditSampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RedditSampleApp
+//
+//  Created by rogerio on 10/08/20.
+//  Copyright © 2020 rogerio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
